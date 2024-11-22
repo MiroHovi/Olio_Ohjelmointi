744cd1a6307356004316882704701ba0518f2710
MiroHovi <t3homi01@students.oamk.fi>
2024-11-22 20:09:56 +0200

    h3a valmis

--- h3a/main.cpp
+++ h3a/main.cpp
@@ -3,17 +3,21 @@
 
 int main() {
     {
+        // Ensimmäinen lohko: Chef Gordon Ramsay
         Chef chef("Gordon Ramsay");
         chef.makeSalad();
         chef.makeSoup();
+        // Chef-olion elinkaari päättyy tässä, ja destruktori kutsutaan
     }
 
     {
+        // Toinen lohko: ItalianChef Anthony Bourdain
         ItalianChef italianChef("Anthony Bourdain");
         italianChef.makeSalad();
         italianChef.makeSoup();
         italianChef.makePasta();
         std::cout << "name of the Italian Chef is " << italianChef.getName() << std::endl;
+        // ItalianChef-olion elinkaari päättyy tässä, ja destruktori kutsutaan
     }
 
     return 0;
